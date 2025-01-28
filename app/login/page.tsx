4ffeb45a0edd1f53022eaff87dffaeaf031db680
denilson <denilson.felipe.25@gmail.com>
2025-01-28 12:31:30 -0500

    changed login route location, tidy up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,5 +1,9 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+
  
 export default function LoginPage() {
   return (
@@ -8,6 +12,9 @@ export default function LoginPage() {
       <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
       <div className='flex-1 rounded-lg bg-red-500 p-2 content-center justify-center'>
         <h1 className='text-white flex-1 mb-3 text-md text-center'>Login function still in development...</h1>
+        <div className='text-white align-center flex justify-center hover:bg-red-500'>
+            <Link href='/dashboard'>Go to Dashboard {'->'}</Link>
+        </div>
       </div>
         <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
           
@@ -20,4 +27,3 @@ export default function LoginPage() {
     </main>
   );
 }
-// L13OPdMTKITWVs0OwBpZtXj2BJN6gh1INpQj4DC6Hkc=
